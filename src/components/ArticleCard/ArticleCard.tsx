5c5602d9bb1002a33f1ab2d16a3479a924dd7893
Melissa Astbury <melissaastbury@hotmail.com>
2021-02-14 11:10:54 +0000

    moved the vote icon to the article card component so this is more accessible

--- src/components/ArticleCard/ArticleCard.tsx
+++ src/components/ArticleCard/ArticleCard.tsx
@@ -1,4 +1,5 @@
 import { Article } from '../../types/types';
+import { FaThumbsUp, FaThumbsDown } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import Button from '../Button/Button';
 import './ArticleCard.scss';
@@ -20,6 +21,11 @@ const ArticleList: React.FC<Props> = ({ article }) => {
       <Link to={`/article/${article.article_id}`}>
         <Button btnType="success">READ MORE</Button>
       </Link>
+      <div className="vote">
+        <p>Have your say!</p>
+        <FaThumbsUp />
+        <FaThumbsDown />
+      </div>
     </div>
   );
 };
--- src/containers/SingleArticle/SingleArticle.tsx
+++ src/containers/SingleArticle/SingleArticle.tsx
@@ -1,53 +1,47 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { FaReadme, FaThumbsUp, FaThumbsDown } from "react-icons/fa";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { FaReadme } from 'react-icons/fa';
 
 import { RootState } from '../..';
-import Button from "../../components/Button/Button";
-import Comments from "../../components/Comments/Comments";
-import { getAllArticleByIdStart } from "../../store/actions/articlesAction";
-import "./SingleArticle.scss";
+import Button from '../../components/Button/Button';
+import Comments from '../../components/Comments/Comments';
+import { getAllArticleByIdStart } from '../../store/actions/articlesAction';
+import './SingleArticle.scss';
 
 interface Props {
-    match: { params: { id: string }}
+  match: { params: { id: string } };
 }
 
-const SingleArticle: React.FC<Props> = ({match}) => {
-    const dispatch = useDispatch()
-    const article = useSelector((state: RootState) => state.articleReducer.article)
-    const [articleId] = React.useState(match.params.id);
+const SingleArticle: React.FC<Props> = ({ match }) => {
+  const dispatch = useDispatch();
+  const article = useSelector(
+    (state: RootState) => state.articleReducer.article
+  );
+  const [articleId] = React.useState(match.params.id);
 
-    React.useEffect(() => {
-        dispatch(getAllArticleByIdStart(articleId))
-    }, [dispatch])
+  React.useEffect(() => {
+    dispatch(getAllArticleByIdStart(articleId));
+  }, [dispatch]);
 
-    return (
-        article ? (
-            <>
-            <Link to="/">
-                <Button btnType="slim">Back</Button>
-                </Link>
-                <div className="single-article">
-                    <h3>{article.title}</h3>
-                    <FaReadme size={70} />
-                    <p>{article.author}</p>
-                    <p>{article.topic}</p>
-                    <p>Votes: {article.votes}</p>
-                    <div className="vote">
-                        <p>Have your say!</p>
-                        <FaThumbsUp />
-                        <FaThumbsDown />
-
-                    </div>
-                    <p>Comment Count: {article.comment_count}</p>
-                    <Comments articleId={articleId} />
-                </div>
-            </>
-        ) : (
-            <h3>Sorry could not find your article</h3>
-        )
-    )
+  return article ? (
+    <>
+      <Link to="/">
+        <Button btnType="slim">Back</Button>
+      </Link>
+      <div className="single-article">
+        <h3>{article.title}</h3>
+        <FaReadme size={70} />
+        <p>{article.author}</p>
+        <p>{article.topic}</p>
+        <p>Votes: {article.votes}</p>
+        <p>Comment Count: {article.comment_count}</p>
+        <Comments articleId={articleId} />
+      </div>
+    </>
+  ) : (
+    <h3>Sorry could not find your article</h3>
+  );
 };
 
 export default SingleArticle;
